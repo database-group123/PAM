0af508d1cb2826b42bd413486ffed0fafffc02a5
database-group123 <58168836+database-group123@users.noreply.github.com>
2019-11-26 10:58:41 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/myapplicationmenu/MainActivity.java
+++ app/src/main/java/com/example/myapplicationmenu/MainActivity.java
@@ -1,20 +1,93 @@
 package com.example.myapplicationmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Tworzenie modelu dokumentu do bazy danych. Użycie metody put dodaje kolejną parę klucz wartość do obiektu, który potem możemy dodać no bazy.
+        Map<String, Object> fbModel = new HashMap<>();
+        fbModel.put("nazwa", "Programowanie Aplikacji Mobilnych");
+
+        // Dodanie dokumentu do bazy. Zmienamy w db.collection() parametr na taki jaki nas interesuje by dodac do tej kolekcji, a nastepenie w parametrze metody add dodajemy nasz wczesniej utworzony model
+        db.collection("kursy")
+                .add(fbModel)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        System.out.println(documentReference.getId());
+                        Toast.makeText(MainActivity.this,"Dane do bazy" + documentReference.getId(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println(e);
+                    }
+                });
+
+        // Usuniecie dokumentu z kolekcji. Jaki parametr w metodzie document podajemy unikalny identyfikator dokumentu.
+        db.collection("kursy").document("7Yxz9jRWfXhRSdLLtPnx")
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        System.out.println("Usunieto dokument 7Yxz9jRWfXhRSdLLtPnx");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println(e);
+                    }
+                });
+
+
+        // Pobranie kolekcji z bazy danych. Aby otrzymać inną kolekcję zmieniamy w db.collection() parametr z "klienci" na inny
+        db.collection("klienci")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                System.out.println(document.getData());
+                                Toast.makeText(MainActivity.this,"Dane z bazy" + document.getData(), Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            System.out.println(task.getException());
+                        }
+                    }
+                });
     }
 
     @Override
@@ -31,8 +104,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        //View view = findViewById(R.id.menu_option1);
 
         switch(item.getItemId()) {
             case R.id.menu_option1:
